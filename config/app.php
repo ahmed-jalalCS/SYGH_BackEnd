d268211d1d3fe5c83d6d29581269f7727243ad1b
OuisAlhetar <ouis.alhetar@gmail.com>
2024-12-27 19:04:43 +0300

    add providers to config/app.php

--- config/app.php
+++ config/app.php
@@ -123,4 +123,20 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
+    // !############ Very important ###############
+    'providers' => ServiceProvider::defaultProviders()->merge([
+        /*
+         * Package Service Providers...
+         */
+
+        /*
+         * Application Service Providers...
+         */
+        App\Providers\AppServiceProvider::class,
+        App\Providers\AuthServiceProvider::class,
+        App\Providers\EventServiceProvider::class,
+        // App\Providers\Filament\AdminPanelProvider::class,
+        App\Providers\RouteServiceProvider::class,
+    ])->toArray(),
+
 ];
